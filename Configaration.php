fe74b69d41d23b57ab3579ebcf5bd06729570944
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2020-10-12 14:04:07 +0600

    1st commit

---
+++ Configaration.php
@@ -0,0 +1,20 @@
+<?php
+$host = "localhost"; /* Host name */
+$user = "root"; /* User */
+$password = ""; /* Password */
+$dbname = "project_need_help"; /* Database name */
+
+$connection = mysqli_connect($host, $user, $password,$dbname);
+ //Check connection(uncomment it for check)
+if($connection) {
+    echo " < br>"."We are connected";
+} else {
+    die("Database connection failed");
+}
+if(!mysqli_select_db($connection,$dbname)){
+    echo " < br>"."not selected database";
+}
+else{
+    echo " < br>"."Database Selected"." < br>";
+}
+?>
